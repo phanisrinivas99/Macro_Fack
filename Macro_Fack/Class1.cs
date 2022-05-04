2f1c2929567908ff3605eaac8cb39d9d9d5c4d17
Madhav <g.m.madhav16@gmail.com>
2022-05-04 13:30:55 +0530

    Add project files.

---
+++ Macro_Fack/Class1.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Macro_Fack
+{
+    public static class Macro
+    {
+        public static List<History> GetHeaders()
+        {
+            var connectionString = "Data Source=LAPTOP-OD0O0KPE\\SQLEXPRESS;Initial Catalog=Profile;Integrated Security=true;" ;
+            string query = "select * from LockUnlock";
+            using (var db = new SqlConnection(connectionString))
+                return db.Query<History>(query).AsList();
+        }
+        public static List<Details> GetDetails()
+        {
+            var connectionString = "Data Source=LAPTOP-OD0O0KPE\\SQLEXPRESS;Initial Catalog=Profile;Integrated Security=true;";
+            string query = "select * from LockUnlockDetails";
+            using (var db = new SqlConnection(connectionString))
+                return db.Query<Details>(query).AsList();
+        }
+        public static List<Details> GetDetailsById(int id)
+        {
+            var connectionString = "Data Source=LAPTOP-OD0O0KPE\\SQLEXPRESS;Initial Catalog=Profile;Integrated Security=true;";
+            string query = "select * from LockUnlockDetails where LockUnlock_Id=" + id;
+            using (var db = new SqlConnection(connectionString))
+                return db.Query<Details>(query).AsList();
+        }
+    }
+}
---
+++ Macro_Fack/History.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Macro_Fack
+{
+    public class History
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Lock_Unlock { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+    public class Details
+    {
+        public string Id { get; set; }
+        public string LockUnlock_Id { get; set; }
+        public string Division { get; set; }
+        public string ProfileName { get; set; }
+        public string LockUnlock_Status { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
